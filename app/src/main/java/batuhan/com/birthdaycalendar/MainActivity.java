17050038fcab359202d155bf083e8e110362497c
Batuhan <batuhankoklu@hotmail.com>
2019-04-29 00:40:56 +0300

    First Commit, CalendarView and RecylerView added,Adapter,Model created.

--- app/src/main/java/batuhan/com/birthdaycalendar/MainActivity.java
+++ app/src/main/java/batuhan/com/birthdaycalendar/MainActivity.java
@@ -1,13 +1,106 @@
 package batuhan.com.birthdaycalendar;
 
+import android.graphics.Color;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.v7.widget.SearchView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import batuhan.com.birthdaycalendar.Adapters.AdapterBirthdayCardView;
+import batuhan.com.birthdaycalendar.Models.BirthdayModel;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private android.support.v7.widget.Toolbar toolbar;
+    private AdapterBirthdayCardView adapter;
+    private ArrayList<BirthdayModel> birthdayModelList;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        birthdayModelList = new ArrayList<BirthdayModel>();
+
+        BirthdayModel model = new BirthdayModel(1,"Batuhan Köklü'nün doğum günü","Bodrumda Kutlanacak","22.07.1997",0);
+        birthdayModelList.add(model);
+
+        adapter = new AdapterBirthdayCardView(getApplicationContext(),birthdayModelList);
+        recyclerView.setAdapter(adapter);
+
     }
+
+    //Toolbar entegresi
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+
+        //Search Entegresi
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(this);
+
+
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_add:
+                Toast.makeText(getApplicationContext(),"Add New Birth",Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    //Toolbar Search
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        Log.e("TextSubmit",query);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        Log.e("TextChanged",newText);
+
+        return true;
+    }
+
+    /*public BirthdayModel createModel(){
+
+    }*/
+
 }
